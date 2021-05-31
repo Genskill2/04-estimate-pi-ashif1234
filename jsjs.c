0a236b6c68e158109ed5ddbad04f35850250be8a
ashif1234 <85074815+ashif1234@users.noreply.github.com>
2021-05-31 12:08:23 +0530

    Update jsjs.c

--- jsjs.c
+++ jsjs.c
@@ -7,10 +7,12 @@ float wallis_pi(int);
 
 int main(void) {
   float pi;
-  for (int i=0; i<5; i++) {
+  for (int i=0; i<=5; i++) {
     pi = wallis_pi(i);
+    //printf("wallis called %f %f\n",M_PI,pi) ;
     if (!(fabs(pi - M_PI) > 0.15)) {
-      printf("Estimate with just %d iterations is %f which is too accurate.\n", i, pi);
+      //printf("wallis g %f\n",pi) ;
+  printf("Estimate with just %d iterations is %f which is too accurate.\n", i, pi);
           abort();
     }
   }
@@ -23,21 +25,25 @@ int main(void) {
     }
   }
   
-}
+} 
 
 
 float wallis_pi(int n)
 {
-    float pi=1.0;
+float pi=1.0;
    int i;
     for(i=1;i<=n;i++) 
      {
       // printf("%d ",i);
        
         pi *=(4.0*i*i)/(4.0*i*i-1);
-  //printf("%f\n",pi);
-      }
-
-  return pi*2;
+       }
+if(n!=0)
+ pi*=2.0; 
+ else pi=0.0;
+// printf("%f..\n",pi);
+ 
+  return (pi);
 }
+    
     
